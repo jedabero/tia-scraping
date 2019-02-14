073cd9c800e15608e123f1a1a4c0f15cdaeea684
Jeison Berdugo <jeison.berdugo@pluriza.com>
2019-02-14 11:26:25 -0500

    Fixed getData to fetch more results

--- functions/index.js
+++ functions/index.js
@@ -16,47 +16,55 @@ exports.getData = functions.https.onRequest(async (req, res) => {
   } else {
     res.set('Access-Control-Allow-Origin', allowedOrigins[2]);
   }
-  const { rad = 5, rad2 = 'km', rad3 = '', cid = 1 } = req.query;
-  const result = await request({
-    uri: `${SITE}/SearchResult.asp?rad=${rad}&rad2=${rad2}&rad3=${rad3}&cid=${cid}`,
-    method: 'GET',
-    family: 4,
-  });
-  const page = cheerio.load(result);
   const listings = [];
-  page('#ListNav').nextAll('#ListingMini').each((i, listing) => {
-    const listingNode = page(listing).first().first().first();
-    const companySectionNode = listingNode.find('#companyname');
-    const companyNode = companySectionNode.children().first();
-    const companyURI = `${SITE}/${companyNode.attr('href')}`;
-    const companyName = companyNode.text();
-    const ownerNode = companySectionNode.next();
-    const owner = ownerNode.text();
-    let poBox, fax, website, address = '';
-    ownerNode.nextAll('br').each((i, brNode) => {
-      let textAfterBr = page(brNode).get(0).nextSibling.nodeValue;
-      textAfterBr = textAfterBr ? textAfterBr.trim() : textAfterBr;
-      if (textAfterBr && textAfterBr.includes('P.O. Box')) {
-        poBox = textAfterBr;
-      }
-      if (textAfterBr && textAfterBr.includes('Fax')) {
-        fax = textAfterBr;
-      }
-      if (textAfterBr && textAfterBr.includes('www.')) {
-        website = textAfterBr;
-      }
-      if (textAfterBr && !textAfterBr.includes('P.O. Box') && !textAfterBr.includes('Phone') && !textAfterBr.includes('Fax') && !textAfterBr.includes('www.')) {
-        address += `${address.length > 0 ? ', ' : ''}${textAfterBr}`;
-      }
+  const { rad = 5, rad2 = 'km', rad3 = '', cid = 1 } = req.query;
+  let nextPageURI = `SearchResult.asp?rad=${rad}&rad2=${rad2}&rad3=${rad3}&cid=${cid}`;
+  do {
+    /* eslint-disable no-await-in-loop */
+    const result = await request({
+      uri: `${SITE}/${nextPageURI}`,
+      method: 'GET',
+      family: 4,
     });
-    address = address.trim();
+    const page = cheerio.load(result);
+    const listNav = page('#ListNav');
+    const nextPageURINode = listNav.children().children().children().children().last();
+    nextPageURI = nextPageURINode.children().children().first().attr('href');
+    listNav.nextAll('#ListingMini').each((i, listing) => {
+      const listingNode = page(listing);
+      const companySectionNode = listingNode.find('#companyname');
+      const companyNode = companySectionNode.children().first();
+      const companyURI = `${SITE}/${companyNode.attr('href')}`;
+      const companyName = companyNode.text();
+      const ownerNode = companySectionNode.next();
+      const owner = ownerNode.text();
+      let poBox, fax, website, address = '';
+      ownerNode.nextAll('br').each((i, brNode) => {
+        let textAfterBr = page(brNode).get(0).nextSibling.nodeValue;
+        textAfterBr = textAfterBr ? textAfterBr.trim() : textAfterBr;
+        if (textAfterBr && textAfterBr.includes('P.O. Box')) {
+          poBox = textAfterBr;
+        }
+        if (textAfterBr && textAfterBr.includes('Fax')) {
+          fax = textAfterBr;
+        }
+        if (textAfterBr && textAfterBr.includes('www.')) {
+          website = textAfterBr;
+        }
+        if (textAfterBr && !textAfterBr.includes('P.O. Box') && !textAfterBr.includes('Phone') && !textAfterBr.includes('Fax') && !textAfterBr.includes('www.')) {
+          address += `${address.length > 0 ? ', ' : ''}${textAfterBr}`;
+        }
+      });
+      address = address.trim();
 
-    const phone = ownerNode.nextAll('.Disappear').first().text();
-    const email = ownerNode.nextAll('a').first().text();
-    const distance = ownerNode.nextAll('i').text();
+      const phone = ownerNode.nextAll('.Disappear').first().text();
+      const email = ownerNode.nextAll('a').first().text();
+      const distance = ownerNode.nextAll('i').text();
 
-    listings[i] = { companyName, companyURI, owner, address, poBox, fax, website, phone, email, distance };
-  });
+      listings.push({ companyName, companyURI, owner, address, poBox, fax, website, phone, email, distance });
+    });
 
-  res.json(listings);
+  } while (nextPageURI);
+
+  res.json({ length: listings.length, listings });
 });
